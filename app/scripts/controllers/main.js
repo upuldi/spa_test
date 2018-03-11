d520655b3c1e8cb25ed330f78ecddbf69b2587a1
upuldi <upuldi@gmail.com>
2018-03-11 16:35:30 +1100

    added basic scaffloaded angular project with grunt

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name spaTestApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the spaTestApp
+ */
+angular.module('spaTestApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
